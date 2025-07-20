c63d0b003f66fe520d7bc821df03813d4d082e95
victormba-kalu <mbavictorkalu@gmail.com>
2025-07-20 14:58:39 -0400

    added more styling and scripts to improve the responsiveness of the directory

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -13,9 +13,10 @@ const displayBusiness = (businesses) => {
 
         let card = document.createElement('section');
         let logo = document.createElement('img');
+        let businessName = document.createElement('h2');
         let address = document.createElement('p');
         let number = document.createElement('p');
-        let website = document.createElement('p');
+        let website = document.createElement('a');
 
         logo.setAttribute('src', business.image);
         logo.setAttribute('alt', `This business belongs to ${business.name}`);
@@ -23,11 +24,16 @@ const displayBusiness = (businesses) => {
         logo.setAttribute('width', '100');
         logo.setAttribute('height', '100');
 
+        website.setAttribute('href', business.website);
+        website.setAttribute('target', '_blank');
+
+        businessName.textContent = business.name;
         address.textContent = business.address;
         number.textContent = business.phone;
         website.textContent = business.website;
 
         card.appendChild(logo);
+        card.appendChild(businessName);
         card.appendChild(address);
         card.appendChild(number);
         card.appendChild(website);
