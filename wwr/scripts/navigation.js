3dfd781a77c0f74a33815cbb9fa979477780ea01
victormba-kalu <mbavictorkalu@gmail.com>
2025-08-14 00:46:08 -0400

    added more css and update the navigation.js codes to automatically take care of wayfinding

--- wwr/scripts/navigation.js
+++ wwr/scripts/navigation.js
@@ -1,7 +1,23 @@
 const navButton = document.querySelector('#nav-button');
 const navBar = document.querySelector('#nav-bar');
+const navLinks = navBar.querySelectorAll('ul li a');
 
 navButton.addEventListener('click', () => {
-    navButton.classList.toggle('show');
-    navBar.classList.toggle('show');
+    navButton.classList.toggle('open'); 
+    navBar.classList.toggle('open');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const currentPagePath = window.location.pathname;
+
+    navLinks.forEach(link => {
+        link.closest('li').classList.remove('current'); 
+
+        const linkPath = new URL(link.href).pathname;
+
+        if (currentPagePath === linkPath || 
+            (currentPagePath === '/' && linkPath === '/index.html')) {
+            link.closest('li').classList.add('current');
+        }
+    });
 });
